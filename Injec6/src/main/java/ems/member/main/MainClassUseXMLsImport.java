d2f172cee26b3724b5669df2a59a5bd0b9ca617a
admoonistrator <91christian83@gmail.com>
2022-07-30 17:41:19 +0900

    appCtx3.xml

--- Injec6/src/main/java/ems/member/main/MainClassUseXMLsImport.java
+++ Injec6/src/main/java/ems/member/main/MainClassUseXMLsImport.java
@@ -1,8 +1,21 @@
 package ems.member.main;
 
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ems.member.service.EMSInformationService;
+import ems.member.service.StudentRegisterService;
+
 public class MainClassUseXMLsImport {
 
 	public static void main(String[] args) {
+		
+		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext("classpath:appCtxImport.xml");
+		
+		EMSInformationService informationService=ctx.getBean("informationService",EMSInformationService.class);
+		informationService.outputEMSInformation();
+		
+		StudentRegisterService registerService = ctx.getBean("registerService", StudentRegisterService.class);
+		
 		
 	}
 
