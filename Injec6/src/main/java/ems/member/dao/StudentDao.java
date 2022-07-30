3e050e144a7e7a18096234f6c3319fbf6ce8c828
admoonistrator <91christian83@gmail.com>
2022-07-30 22:00:43 +0900

    appCtx2.xml

--- Injec6/src/main/java/ems/member/dao/StudentDao.java
+++ Injec6/src/main/java/ems/member/dao/StudentDao.java
@@ -1,7 +1,20 @@
 package ems.member.dao;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import ems.member.Student;
 
 public class StudentDao {
 
+	private Map<String, Student> studentDB = new HashMap<String, Student>();
 	
+	
+	public Student select(String sNum) {
+		return studentDB.get(sNum);
+	}
+	
+	public  void insert(Student student) {
+		studentDB.put(student.getsNum(), student);
+	}
 }
